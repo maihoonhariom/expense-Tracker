e580f038a736000cb349b239e002954ed727127c
maihoonhariom <rajputhariom835@gmail.com>
2024-09-04 09:26:07 +0530

    Starting of work on expense tracker

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+require("dotenv").config({path: "./.env"});
+app.use(require("morgan")("combined"));  // morgan
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+const indexRouter = require("./routes/index");
+
+
+app.use("/", indexRouter);
+
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server is running on PORT: ${process.env.PORT}`);
+    
+})
